84a2807361682f1b83236d9d74d71cc93f4ef08b
GREED <pratikadhikari10@gmail.com>
2024-02-08 10:15:27 +0100

    g

--- obj/Debug/net8.0/C# SCHOOL Rotterdam.AssemblyInfo.cs
+++ obj/Debug/net8.0/C# SCHOOL Rotterdam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# SCHOOL Rotterdam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a061b738893d45c1a51beb0e1a4c3e4d4fcc68e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e523e94f3c9f049a04d2a3c0c39e6351c086394")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# SCHOOL Rotterdam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# SCHOOL Rotterdam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
